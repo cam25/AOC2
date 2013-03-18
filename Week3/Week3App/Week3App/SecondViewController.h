3fdc2f4061124c50c56ff95cddd59344e497c4be
Cameron Mozie <cam25@fullsail.edu>
2013-03-18 01:08:53 -0400

    Second View added

---
+++ Week3/Week3App/Week3App/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Week3App
+//
+//  Created by Cameron Mozie on 3/18/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
