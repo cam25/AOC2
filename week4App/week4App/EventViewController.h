17238aa8b696980891b3ac6f9ce5ed33d5f49ce1
Cameron Mozie <cam25@fullsail.edu>
2013-03-21 22:08:30 -0400

    Added Event View controller

---
+++ week4App/week4App/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  week4App
+//
+//  Created by Cameron Mozie on 3/21/13.
+//  Copyright (c) 2013 Cameron Mozie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
